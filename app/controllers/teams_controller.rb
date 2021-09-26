5676a42e6557f2319c5a22447744de01bed1134b
Pablo Fernández Casal <pablofcasal@gmail.com>
2021-09-26 17:11:49 +0200

    Team model full CRUD

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,21 +1,48 @@
 class TeamsController < ApplicationController
 
   def index
-    @team = Team.all
-  end
-
-  def create
-    @team = Team.new(team_params)
-    if @team.save
-      redirect_to @team, notice: "Team was successfully created."
-    else
-      render :new
-    end
+    @teams = Team.all
   end
 
   def new
     @team = Team.new
   end
+
+  def create
+    @team = Team.new(team_params)
+    if @team.save
+      redirect_to teams_path, notice: "Team was successfully created."
+    else
+      render :new
+    end  
+  end
+
+  def show
+    @team = Team.find(params[:id])
+  end
+
+
+  def edit
+    @team = Team.find(params[:id])
+  end
+
+  def update
+    @team = Team.find(params[:id])
+    if @team.update(team_params)
+      redirect_to teams_path, notice: "Team was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+
+  def destroy
+    @team = Team.find(params[:id])
+    @team.destroy
+    redirect_to teams_path, notice: "Team was successfully destroyed."
+  end
+
+
 
   private
 
