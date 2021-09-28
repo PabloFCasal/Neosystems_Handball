596b63774b762e065ad96bec6f4afdc10f6c4553
Pablo Fernández Casal <pablofcasal@gmail.com>
2021-09-28 21:40:35 +0200

    Almost finished

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,14 @@
 class Team < ApplicationRecord
 
+  has_many :local_matches, class_name: "Match", foreign_key: "local_team_id"
+  has_many :visiting_matches, class_name: "Match", foreign_key: "visiting_team_id"
   
   validates :name, presence: true, uniqueness: true
+
+  def can_be_destroyed?
+    
+    local_matches.empty? && visiting_matches.empty?
+
+  end
 
 end
